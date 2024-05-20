265f97316b5c8867182d1ee9c8da430d99d13d19
Salimiyan <amirali16salimiyan@gmail.com>
2024-05-20 14:17:43 +0330

    The background color of the cat logo has changed so the EnterActivity works better in Dark Mode

--- app/src/main/java/com/amir/test/SplashActivity.kt
+++ app/src/main/java/com/amir/test/SplashActivity.kt
@@ -24,6 +24,6 @@ class SplashActivity : AppCompatActivity() {
             val intent=Intent(this@SplashActivity,EnterActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        },2000)
     }
 }
